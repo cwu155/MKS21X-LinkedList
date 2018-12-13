c58fc6a36b2bc13dec026c4d25f1dff8dca382fe
cwu155 <cwu17@stuy.edu>
2018-12-13 14:45:05 -0500

    Added Node class

---
+++ MyLinkedList.java
@@ -0,0 +1,31 @@
+public class MyLinkedList{
+  private class Node{
+    private Node next, prev;
+    private Integer data;
+
+    public Node next(){
+      return next;
+    }
+    public Node prev(){
+      return prev;
+    }
+    public void setNext(Node other){
+      next = other;
+    }
+    public void setPrev(Node other){
+      prev = other;
+    }
+    public Integer getData(){
+      return data;
+    }
+    public Integer setData(Integer i){
+      data = i;
+      return data;
+    }
+    public String toString(){
+      return Integer.toString(data);
+    }
+
+
+  }
+}
