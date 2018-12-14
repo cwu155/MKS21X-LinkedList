7b33b2cba10fe9d2bd9a3d2dfcf6af77ad618dd7
cwu155 <cwu17@stuy.edu>
2018-12-14 14:47:35 -0500

    Added MyLinkedList constructor

---
+++ Driver.java
@@ -0,0 +1,12 @@
+public class Driver {
+    public static void main(String[] args) {
+
+        //Step One: Creating a valid Instance
+        System.out.println("######################");
+        System.out.println("##New Instance Test:##");
+        System.out.println("######################");
+        MyLinkedList list = new MyLinkedList();
+        System.out.println(list);
+        System.out.println("This should print: \"[]\"\n");
+      }
+    }
--- MyLinkedList.java
+++ MyLinkedList.java
@@ -3,6 +3,10 @@ public class MyLinkedList{
     private Node next, prev;
     private Integer data;
 
+    public Node (start, end){
+      prev = start;
+      next = end;
+    }
     public Node next(){
       return next;
     }
@@ -25,7 +29,36 @@ public class MyLinkedList{
     public String toString(){
       return Integer.toString(data);
     }
+    }
+
+    private Node start;
+    private Node end;
+    private int length;
+
+    public MyLinkedList(){
+      start = null;
+      end = null;
+      length = 0;
+    }
+
+    public boolean add(Integer value){
+        Node newNode = new Node(start, end);
+        length += 1;
+        return true;
+    }
+
+    public int size(){
+      return length;
+    }
+
+    // public String toString(){
+    //   String result = "";
+    //   for (int i = 1; i < length-1; i++){
+    //     result += this.getData();
+    //   }
+    //   return "[" + start + result + end + "]";
+    // }
 
 
-  }
+
 }
